173748d9be7424d007341d580d99d7e81797007b
aipa <k.tamashiro@churadata.okinawa>
2023-01-05 20:56:35 +0900

    [fix] 圧縮の手順が間違えてた.

--- src/add.rb
+++ src/add.rb
@@ -9,18 +9,19 @@ def add(file_path)
         file_size = f.size
     end
 
+    # git objetのフォーマット: `Objectの種類 データのサイズ\0データ`
     header = "blob #{file_size}\0"
     store = header + content
     sha1 = Digest::SHA1.new
     sha1.update(store)
+    # 検索向上のために先頭2文字のディレクトリを用意する
     sub_dir_name = sha1.hexdigest[0, 2]
     object_file_name = sha1.hexdigest[2, 40]
 
     # 圧縮ファイルを作成
     # save_object_path = "./git/objects/#{sub_dir_name}/#{object_file_name}"
     save_object_path = "./#{sub_dir_name}_#{object_file_name}"
-    File.open(save_object_path, 'w') do |f|
-        gz = Zlib::GzipWriter.new(f)
+    Zlib::GzipWriter.open(save_object_path) do |gz|
         gz.write(store)
     end
 
