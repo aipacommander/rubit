99af96923c89ff788d30b6982b5b51a567365fb4
aipa <k.tamashiro@churadata.okinawa>
2023-01-05 21:05:01 +0900

    [update] file typeを表示するように修正.

--- src/cat_file.rb
+++ src/cat_file.rb
@@ -2,13 +2,19 @@ require 'zlib'
 
 def cat_file(option, hash_value)
     if option == '-t'
+        store = ''
         Zlib::GzipReader.open(hash_value) do |gz|
-            puts gz.read
+            store = gz.read
         end
 
-        return '-t'
+        # ファイルフォーマットの次の文字が空文字なので
+        # 最初の空文字の位置を探す
+        file_type_last_index = store.index(/\s/)
+        # 0~から最初の空文字を含まないところまでslice
+        file_type = store.slice(0...file_type_last_index)
+        puts file_type
     elsif option == '-p'
-        return '-p'
+        puts '-p'
     else
         puts 'そのようなoptionは存在しません'
     end
